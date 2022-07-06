c1fd6f07779166f252e143dcee6b94b583280e64
Joseph Kariuki <joehene@gmail.com>
2022-07-06 11:47:07 +0300

    Added PostGIS database engine variable.

--- mywebgis/settings.py
+++ mywebgis/settings.py
@@ -132,3 +132,4 @@ if ENVIRONMENT == 'production':
 # Database
 db_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_env)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
