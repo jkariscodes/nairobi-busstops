44d3a4298d3a7ce93b4bcc0476941874839c985e
Joseph Kariuki <joehene@gmail.com>
2023-06-24 18:28:01 +0300

    Removed generated staticfiles.

--- staticfiles/js/main.fc6cee0db189.js
+++
@@ -1,8 +0,0 @@
-function main_map_init (map, options) {
-    let dataurl = '{% url "data" %}';
-    // Download GeoJSON via Ajax
-    $.getJSON(dataurl, function (data) {
-        // Add GeoJSON layer
-        L.geoJson(data).addTo(map);
-    });
-}
