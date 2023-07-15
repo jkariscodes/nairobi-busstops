7996398dcdb96e01147efd2aa6592eb33c57ed4e
Joseph Kariuki <joehene@gmail.com>
2023-07-15 05:53:28 +0300

    Front end (#26)
    * Updated list of files to ignore.
    
    * Created configuration for front-end service.
    
    * Updated allowed cross-origin list.
    
    * Created url configuration for api.
    
    * Created front-end app.
    
    * Added heading element.

--- mywebgis/settings/base.py
+++ mywebgis/settings/base.py
@@ -25,14 +25,21 @@ INSTALLED_APPS = [
     "django.contrib.gis",
     "rest_framework",
     "rest_framework_gis",
+    "corsheaders",
     "leaflet",
     "djgeojson",
     "onyeshamap",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1",
+    "http://127.0.0.1:443",
+]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
